dfc78ff441900f2955cb0a07a9b099ca534d1d26
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-22 15:34:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "google" {
+  access_token = var.access_token
+}
+
+
+resource "google_dataproc_cluster" "mycluster" {
+  name     = "mycluster"
+  region   = "us-central1"
+  graceful_decommission_timeout = "120s"
+  labels = {
+    foo = "bar"
+  }
+
+  cluster_config {
+    staging_bucket = "dataproc-staging-bucket"
+
+    master_config {
+      num_instances = 1
+      machine_type  = "e2-medium"
+      disk_config {
+        boot_disk_type    = "pd-ssd"
+        boot_disk_size_gb = 30
+      }
+    }
+
+    worker_config {
+      num_instances    = 2
+      machine_type     = "e2-medium"
+      min_cpu_platform = "Intel Skylake"
+      disk_config {
+        boot_disk_size_gb = 30
+        num_local_ssds    = 1
+      }
+    }
+
+    preemptible_worker_config {
+      num_instances = 0
+    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "1.3.7-deb9"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+    }
+
+  }
+}
